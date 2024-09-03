26cf1e49562e23fcb1f68b489024c6a0fa366e11
rrirrirr <drpihl@gmail.com>
2024-09-03 15:16:35 +0200

    sign up page

---
+++ ditt-horoskop/src/app/(app)/signup/page.tsx
@@ -0,0 +1,10 @@
+import { SignupCard } from '@/components/ui/signup'
+import { Fragment } from 'react'
+
+export default function Page() {
+  return (
+    <Fragment>
+      <SignupCard />
+    </Fragment>
+  )
+}
